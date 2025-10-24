30e27264666e61b8ed133b7e8971d21b1240da81
Sujal <sujalb.is24@rvce.edu.in>
2025-10-24 17:54:08 +0530

    remove unwanted files

--- supabase/migrations/002_add_role_enum.sql
+++
@@ -1,7 +0,0 @@
--- Create ENUM type for user roles
-CREATE TYPE user_role AS ENUM ('student', 'admin');
-
--- Alter the profiles table to use the ENUM type
-ALTER TABLE public.profiles 
-  ALTER COLUMN role TYPE user_role USING role::user_role,
-  ALTER COLUMN role SET DEFAULT 'student'::user_role;
